4f27b98a58235572e7cb5b67bd5f24c3c9c6ac34
Ashwin Gattani <ashwin.gattani@ril.com>
2019-06-24 11:33:13 +0530

    Side menu implementation [in-progress]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,25 @@ export class AppComponent {
   title = 'JioANPR';
   previousSelectedMenuId = "-1"
 
-  dataSet = [
-    {name: "Home", src: "../assets/Home.png"},
-    {name: "User Management", src: "../assets/UserManagement.png"},
-    {name: "Health Monitor", src: "../assets/HealthMonitor.png"},
-    {name: "Stats", src: "../assets/Stats.png"},
-    {name: "Settings", src: "../assets/Setting.png"},
-    {name: "Support", src: "../assets/Support.png"},
-    {name: "Help", src: "../assets/Help.png"}
+  menuItems = [
+    {name: "Home", src: "../assets/Home.png", selectedSrc: "../assets/HomeSelected.png"},
+    {name: "User Management", src: "../assets/UserManagement.png", selectedSrc: "../assets/UserManagementSelected.png"},
+    {name: "Health Monitor", src: "../assets/HealthMonitor.png", selectedSrc: "../assets/HealthMonitorSelected.png"},
+    {name: "Stats", src: "../assets/Stats.png", selectedSrc: "../assets/StatsSelected.png"},
+    {name: "Settings", src: "../assets/Setting.png", selectedSrc: "../assets/SettingSelected.png"},
+    {name: "Support", src: "../assets/Support.png", selectedSrc: "../assets/SupportSelected.png"},
+    {name: "Help", src: "../assets/Help.png", selectedSrc: "../assets/HelpSelected.png"}
   ]
+
+  healthItems = [ "CAMERA", "LPU", "LED" ];
 
   menuItemSelected(id: string) {
     document.getElementById(id).style.backgroundColor = "#F35561";
+    (<HTMLImageElement>document.getElementById("img"+id)).src = this.menuItems[id]["selectedSrc"];
+
     if (parseInt(this.previousSelectedMenuId) >= 0 ) {
       document.getElementById(this.previousSelectedMenuId).style.backgroundColor = "transparent";
+      (<HTMLImageElement>document.getElementById("img"+this.previousSelectedMenuId)).src = this.menuItems[this.previousSelectedMenuId]["src"];
     }
 
     this.previousSelectedMenuId = id;
@@ -32,5 +37,9 @@ export class AppComponent {
   logoutClicked() {
     console.log("Logout Clicked");
     
+  }
+
+  getHealthIcon(id: string) {
+    return "../assets/GoodHealth.png"
   }
 }
