43f453f7a35d4e91eaeafbff92babf8f4a3be8e7
Atinderpal Singh <atinder007.singh@gmail.com>
2019-06-24 18:20:34 +0530

    Home Screen Implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  cameraList:string[] = ["Gate A","Gate B"]
+  cameraPlaceHolder: string = "Select Camera"
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeLocation(value) {
+      console.log(value);
+      
   }
 
 }
