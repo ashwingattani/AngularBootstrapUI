552569e1ad6e50a7c15a154f041498746927cfea
Ashwin Gattani <ashwin.gattani@ril.com>
2019-06-26 11:47:40 +0530

    code refactor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,13 @@ export class HomeComponent implements OnInit {
   cameraList:string[] = ["Gate A","Gate B"]
   cameraPlaceHolder: string = "Select Camera"
 
+  headerItems = [
+    {"name": "ENTRY", "value": "100", "src": "../assets/Entry.png", "color": "#59BE94"},
+    {"name": "EXIT", "value": "25", "src": "../assets/Exit.png", "color": "#F35561"},
+    {"name": "UNREGISTERED", "value": "04", "src": "../assets/UnRegistered.png", "color": "#11C1D1"},
+    {"name": "ALERTS", "value": "05", "src": "../assets/Alert.png", "color": "#6CBDE9"},
+  ]
+
   constructor() { }
 
   ngOnInit() {
